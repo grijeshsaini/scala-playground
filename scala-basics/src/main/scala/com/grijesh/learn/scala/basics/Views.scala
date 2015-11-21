e7abfc23278e634e557c3a784b42e6b79facd8e2
grijesh <grijesh.rajoria@gmail.com>
2015-11-21 18:07:57 +0530

    views

---
+++ scala-basics/src/main/scala/com/grijesh/learn/scala/basics/Views.scala
@@ -0,0 +1,15 @@
+package com.grijesh.learn.scala.basics
+
+/**
+ * Created by grijesh on 21/11/15.
+ */
+object Views extends App{
+
+  val view = (1 to 10).view
+  println(view(2))
+
+  println(view(9))
+
+
+
+}
